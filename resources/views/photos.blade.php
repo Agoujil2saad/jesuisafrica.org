5e87c0c3a13029234d32cdfeaa341216a46c3488
agoujil saad <agoujilsaad@MacBook-Pro-de-pro-2.local>
2017-09-22 13:47:34 +0100

    resizing photos

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -183,7 +183,7 @@
 
 <div class="row" id="present">
     <div class="twelve wide column">
- <h1 class="ui header"> Phototéque</h1>
+ <h1 class="ui header">Phototéque</h1>
     </div>
 </div>
 
@@ -216,9 +216,9 @@
 				<h1 class="clearfix" id="galery1">Galerie 1</h1>
 				<ul id="lightgallery_1" class="list-unstyled row">
 					@for ($i = 1; $i < 50; $i++)
-					<li class=""  data-src="images/galery/event1/event_one_{{$i}}.jpg" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
+					<li class=""  data-src="{{asset('images/galery/event1/event_one_'.$i.'.jpg')}}" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
 						<a href="">
-							<img class="img-responsive" src="images/galery/event1/thumbnails/event_one_{{$i}}_tn.jpg">
+							<img class="img-responsive" src="{{asset('images/galery/event1/thumbnails/event_one_'.$i.'_tn.jpg')}}">
 						</a>
 					</li>
 					@endfor
@@ -229,9 +229,9 @@
 						<h1 class="clearfix" id="galery2">Galerie 2</h1>
 				<ul id="lightgallery_2" class="list-unstyled row">
 					@for ($i = 1; $i < 50; $i++)
-					<li class=""  data-src="images/galery/event2/event_two_{{$i}}.jpg" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
+					<li class=""  data-src="{{asset('images/galery/event2/event_two_'.$i.'.jpg')}}" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
 						<a href="">
-							<img class="img-responsive" src="images/galery/event2/thumbnails/event_two_{{$i}}_tn.jpg">
+							<img class="img-responsive" src="{{asset('images/galery/event2/thumbnails/event_two_'.$i.'_tn.jpg')}}">
 						</a>
 					</li>
 					@endfor
@@ -242,9 +242,9 @@
 				<h1 class="clearfix" id="galery3">Galerie 3</h1>
 				<ul id="lightgallery_3" class="list-unstyled row">
 					@for ($i = 1; $i < 50; $i++)
-					<li class=""  data-src="images/galery/event3/event_three_{{$i}}.jpg" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
+					<li class=""  data-src="{{asset('images/galery/event3/event_three_'.$i.'.jpg')}}" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
 						<a href="">
-							<img class="img-responsive" src="images/galery/event3/thumbnails/event_three_{{$i}}_tn.jpg">
+							<img class="img-responsive" src="{{asset('images/galery/event3/thumbnails/event_three_'.$i.'_tn.jpg')}}">
 						</a>
 					</li>
 					@endfor
@@ -255,9 +255,9 @@
 					<h1 class="clearfix" id="galery4">Galerie 4</h1>
 				<ul id="lightgallery_4" class="list-unstyled row">
 					@for ($i = 1; $i < 34; $i++)
-					<li class=""  data-src="images/galery/event4/event_four_{{$i}}.jpg" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
+					<li class=""  data-src="{{asset('images/galery/event4/event_four_'.$i.'.jpg')}}" data-sub-html="<h4>Association Internationale Clé Pour la Paix</h4>">
 						<a href="">
-							<img class="img-responsive" src="images/galery/event4/thumbnails/event_four_{{$i}}_tn.jpg">
+							<img class="img-responsive" src="{{asset('images/galery/event4/thumbnails/event_four_'.$i.'_tn.jpg')}}">
 						</a>
 					</li>
 					@endfor
