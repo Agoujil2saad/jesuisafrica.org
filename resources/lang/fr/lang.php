db91c74bfa36cf5bf00c580b2b05a095ce8cb1f0
Agoujil2saad <agoujilsaad@gmail.com>
2017-10-17 15:16:00 +0100

    adding partners

--- resources/lang/fr/lang.php
+++ resources/lang/fr/lang.php
@@ -1,19 +1,6 @@
 <?php
 return [
    //home page
-  'slider_one_message' => ' Notre objectif est de vous accompagner <br>dans la réussite de votre projet  <br>',
-
-  'slider_two_message' => ' Label Initiative organise  un festival <br>
-  Cet évènement s’inscrit dans le cadre du programme <br> « Village Durable »,<br> un projet ouvert, flexible et participatif, <br>porté par la société civile. <br>',
-
-  'slider_three_message' => 'Discours de S.M le Roi Mohammed VI, 30 juillet 2015 <br>
-  « En dépit des progrès réalisés par notre pays,<br> ce qui me désole, c’est cette situation de précarité que vivent <br>certains de nos concitoyens dans les régions éloignées et enclavées <br>, surtout sur les sommets de l’Atlas et du Rif,<br> les zones sahariennes arides et les oasis <br> ainsi que certains villages dans les plaines et sur les côtes. <br>
-  Nous avons conscience de l’ampleur <br>du déficit qui s’est accumulé depuis des décennies <br> dans ces régions, malgré toutes les initiatives <br> et tous les efforts qui ont été déployés. »',
-
-  'message_principale' => ' Vous êtes une association ou une coopérative de la province de <b>KENITRA ?</b> <br>
-  Vous portez un projet répondant á un besoin ?<br>
-  Vous souhaitez participer au développement de votre territoire<br>
-  trouver des partenaires et gagner en visibilité́ ?<br>',
 
   'name_of_app' => 'Association Internationale Clé De La Paix',
   'support_message' => 'vous accompagne <br>et facilite la réalisation de vos projets.',
