bb25641215b126b6a8e8fd7b3c3d994bcf5f3d29
Agoujil2saad <agoujilsaad@gmail.com>
2017-10-30 12:28:14 +0000

    footer mofication

--- resources/views/partials/footer_1.blade.php
+++ resources/views/partials/footer_1.blade.php
@@ -12,64 +12,64 @@
                         <i class="fax icon"></i>+212535502436
                     </p>
                     <p>
-                    <i class="marker icon"></i>ALEM 5 IMM 5 RES -Ahmed 5 N 1 El Bassatine - MÉKNES</p>
-                    <p>
-                        <i class=" world icon"></i>www.jesuisafrica.org <br>
-                    </p>
-                    <p>
-                        <i class="mail icon"></i>aicpds1@gmail.com <br>
-                        <i class="mail icon"></i>aicpds1.presse@gmail.com <br>
-                        <i class="mail icon"></i>Karly172000@gmail.com
-                    </p>
+                        <i class="marker icon"></i>ALEM 5 IMM 5 RES -Ahmed 5 N 1 El Bassatine - MÉKNES</p>
+                        <p>
+                            <i class=" world icon"></i>www.jesuisafrica.org <br>
+                        </p>
+                        <p>
+                            <i class="mail icon"></i>aicpds1@gmail.com <br>
+                            <i class="mail icon"></i>aicpds1.presse@gmail.com <br>
+                            <i class="mail icon"></i>Karly172000@gmail.com
+                        </p>
+                    </div>
                 </div>
-            </div>
-            <div class="five wide column">
-                <h4 class="ui inverted header" id="footer_titles">@lang('lang.learn_more')</h4>
-                <div class="ui inverted link list" id="footer_items">
-                    <a href="/qui_sommes_nous" class="item">@lang('lang.header_us')</a>
-                    <a href="/contact" class="item">@lang('lang.footer_contact')</a>
-                    <a href="/articles" class="item">@lang('lang.footer_articles')</a>
-                    {{--     <a href="/team" class="item">@lang('lang.footer_team')</a> --}}
+                <div class="five wide column">
+                    <h4 class="ui inverted header" id="footer_titles">@lang('lang.learn_more')</h4>
+                    <div class="ui inverted link list" id="footer_items">
+                        <a href="/qui_sommes_nous" class="item">@lang('lang.header_us')</a>
+                        <a href="/contact" class="item">@lang('lang.footer_contact')</a>
+                        <a href="/articles" class="item">@lang('lang.footer_articles')</a>
+                        {{--     <a href="/team" class="item">@lang('lang.footer_team')</a> --}}
+                    </div>
                 </div>
-            </div>
-            <div class="six wide column">
-                <center>
-                
-                <h4 class="ui inverted header" id="footer_titles">@lang('lang.follow_us')</h4>
-                </center>
-                
-                <ul class="footer_list">
-                    <li>
-                        <center>
-                        <a href="https://www.facebook.com/ASSCLEPAIX" target="_blank">
-                            <i class=" inverted facebook large icon"></i>
-                        </a>
-                        <a href="https://www.google.com" target="_blank">
-                            <i class=" inverted google plus large icon"></i>
-                        </a>
-                        <a href="https://www.youtube.com/channel/UCyZwXbK_BgO78XE-A_jjI3w/videos" target="_blank">
-                            <i class=" inverted youtube large icon"></i>
-                        </a>
-                        <a href="https://www.twitter.com/asclepaix" target="_blank">
-                            <i class=" inverted twitter large icon"></i>
-                        </a>
-                        </center>
-                    </li>
+                <div class="six wide column">
+                    <center>
 
-                    @if (Auth::guest())
-                    <center style="margin:2em 0;">
-                        
-                    <a href="{{ route('login') }}" class="ui inverted button ">Espace Admin</a>
-                    @else
-                    
-                    <a href="{{ route('logout') }}" class="ui inverted button"
-                        onclick="event.preventDefault();
-                        document.getElementById('logout-form').submit();">
-                        Se déconnecter
-                    </a>
-                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                        {{ csrf_field() }}
-                    </form>
+                        <h4 class="ui inverted header" id="footer_titles">@lang('lang.follow_us')</h4>
+                    </center>
+
+                    <ul class="footer_list">
+                        <li>
+                            <center>
+                                <a href="https://www.facebook.com/ASSCLEPAIX" target="_blank">
+                                    <i class=" inverted facebook large icon"></i>
+                                </a>
+                                <a href="https://www.google.com" target="_blank">
+                                    <i class=" inverted google plus large icon"></i>
+                                </a>
+                                <a href="https://www.youtube.com/channel/UCyZwXbK_BgO78XE-A_jjI3w/videos" target="_blank">
+                                    <i class=" inverted youtube large icon"></i>
+                                </a>
+                                <a href="https://www.twitter.com/asclepaix" target="_blank">
+                                    <i class=" inverted twitter large icon"></i>
+                                </a>
+                            </center>
+                        </li>
+
+                        @if (Auth::guest())
+                        <center style="margin:2em 0;">
+
+                            <a href="{{ route('login') }}" class="ui inverted button ">Espace Admin</a>
+                            @else
+
+                            <a href="{{ route('logout') }}" class="ui inverted button"
+                            onclick="event.preventDefault();
+                            document.getElementById('logout-form').submit();">
+                            Se déconnecter
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            {{ csrf_field() }}
+                        </form>
                     </center>
                     @endif
                     
@@ -78,8 +78,8 @@
         </div>
         <hr>
         <div id="copyright">Copyright © 2017-@lang('lang.name_of_app')
-            <div class="created_by">Conception et code: <a href="http://www/md-synergy.com">www.MD-SYNERGY.com</a>
+            <div class="created_by">Conception et code: <a href="http://www.md-synergy.com">www.MD-SYNERGY.com</a>
+            </div>
         </div>
     </div>
-</div>
 </div>
